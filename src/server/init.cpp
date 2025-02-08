e29395f29e571b2a904d9150937df1b8f609f8e9
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2025-02-08 05:40:32 -0500

    tcp server done

--- src/server/init.cpp
+++ src/server/init.cpp
@@ -1,4 +1,5 @@
 #include <netinet/in.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <iostream>
 
@@ -10,8 +11,7 @@ int main() {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
 
     // file descriptor is -1 if socket creation failed
-    if (fd < 0)
-    {
+    if (fd < 0) {
         return 1;
     }
 
@@ -24,10 +24,56 @@ int main() {
     addr.sin_port = htons(1234);
     addr.sin_addr.s_addr = htonl(0);
 
+    // binding to the port defined above in the address struct
     int rv = bind(fd, (const sockaddr *)&addr, sizeof(addr));
     if (rv) {
         cerr << "bind failed" << endl;
         exit(1);
+    }
+
+    // socket not created yet
+
+    // create socket with listen
+    // SOMAXCONN is the maximum number of connections
+    // 4096 on Linux
+    rv = listen(fd, SOMAXCONN);
+    if (rv) {
+        cerr << "listen failed" << endl;
+        exit(1);
+    }
+
+    // accept a connection
+    while (1) {
+        struct sockaddr_in client_addr = {};
+        socklen_t addr_len = sizeof(client_addr);
+
+        // accept requires a pointer to the address length as well
+        int conn = accept(fd, (struct sockaddr *)&client_addr, &addr_len);
+
+        if (conn < 0) {
+            cerr << "some error while accepting" << endl;
+            continue;
+        }
+
+        // do something
+        auto respond = [conn] (char* msg) {
+            char reader[64] = {};
+            ssize_t len_read = read(conn, reader, sizeof(reader) - 1);
+
+            if (len_read < 0) {
+                cerr << "some error while reading" << endl;
+                return;
+            }
+
+            cout << "client sent: " << reader << endl;
+
+            write(conn, msg, strlen(msg));
+        };
+
+        char msg[] = "hello world!";
+        respond(msg);
+
+        close(conn);
     }
 
     return 0;
