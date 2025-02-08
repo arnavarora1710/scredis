8bf6ea331ac3cf0be91904deb993b44243b5c62d
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2025-02-08 05:13:58 -0500

    bind done

--- src/server/init.cpp
+++ src/server/init.cpp
@@ -1,4 +1,10 @@
+#include <netinet/in.h>
 #include <sys/socket.h>
+#include <iostream>
+
+using std::cout;
+using std::cerr;
+using std::endl;
 
 int main() {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -9,7 +15,20 @@ int main() {
         return 1;
     }
 
+    // setting SO_REUSEADDR to 1
     int val = 1;
     setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1234);
+    addr.sin_addr.s_addr = htonl(0);
+
+    int rv = bind(fd, (const sockaddr *)&addr, sizeof(addr));
+    if (rv) {
+        cerr << "bind failed" << endl;
+        exit(1);
+    }
+
     return 0;
 }
